4c17f7d4e4212c57c0c16e434d9c3f8baf9dcec0
Neal Krawetz <neal@aeliustech.com>
2024-11-26 11:15:06 -0700

    AAC: Making AAC scanning more robust.

--- src/format-aac.cpp
+++ src/format-aac.cpp
@@ -19,6 +19,20 @@
  The SEAL record can be inserted between any frame!
  You just have to find out where the frame is located. That's the hard part.
  (Unless you just append it to the end of the AAC! FTW!)
+
+ =====
+ Decoding the frame header is detailed in ExifTool AAC.pm:
+ # format of frame header (7 bytes):
+ # SSSS SSSS SSSS ILLC PPRR RRpC CCoh csff ffff ffff fffb bbbb bbbb bbNN
+ # 1111 1111 1111 0001 0110 0000 0100 0000 0000 0101 0101 1111 1111 1100 (eg.)
+ #  S = sync word                o = original/copy
+ #  I = ID                       h = home
+ #  L = layer (00)               c = copyright ID
+ #  C = CRC absent               s = copyright start
+ #  P = profile object type      f = frame length
+ #  R = sampling rate index      b = buffer fullness
+ #  p = private                  N = number of raw data blocks in frame
+ #  C = channel configuration
  ************************************************/
 #include <stdlib.h>
 #include <ctype.h>
@@ -31,6 +45,66 @@
 
 #pragma GCC visibility push(hidden)
 /**************************************
+ _SealIsAACframe(): Is this an AAC frame?
+ There must be 7 bytes available.
+ Returns: frame length or 0 if not a frame.
+ **************************************/
+uint16_t	_SealIsAACframe	(byte *Data)
+{
+  uint16_t u16;
+  /*****
+   Per ExifTool AAC.pm, check reserved bits.
+
+   # format of frame header (7 bytes):
+   byte 0     |1        |2        |3        |4        |5        |6
+   # SSSS SSSS SSSS ILLC PPRR RRpC CCoh csff ffff ffff fffb bbbb bbbb bbNN
+   # 1111 1111 1111 0001 0110 0000 0100 0000 0000 0101 0101 1111 1111 1100 (eg.)
+   #  S = sync word                o = original/copy
+   #  I = ID                       h = home
+   #  L = layer (00)               c = copyright ID
+   #  C = CRC absent               s = copyright start
+   #  P = profile object type      f = frame length
+   #  R = sampling rate index      b = buffer fullness
+   #  p = private                  N = number of raw data blocks in frame
+   #  C = channel configuration
+
+   P cannot be 3  (reserved profile type)
+   R cannot be > 12  (sampling frequency index)
+   f cannot be < 7  (frame length)
+  
+   $raf->Read($buff, 7) == 7 or return 0;
+   return 0 unless $buff =~ /^\xff[\xf0\xf1]/;
+   my @t = unpack('NnC', $buff);
+   return 0 if (($t[0] >> 16) & 0x03) == 3; # (reserved profile type)
+   return 0 if (($t[0] >> 12) & 0x0f) > 12; # validate sampling frequency index
+   my $len = (($t[0] << 11) & 0x1800) | (($t[1] >> 5) & 0x07ff);
+   return 0 if $len < 7;
+   *****/
+
+  // Check sync word
+  u16 = readbe16(Data+0);
+  switch(u16)
+	{
+	case 0xfff0: // no CRC
+	case 0xfff1: // has CRC
+	  break;
+	default:
+	  return(0);
+	}
+
+  u16 = readbe16(Data+2);
+  if ((u16 & 0xc000) == 0xc000) { return(0); } // P cannot be 3
+  if ((u16 & 0x3c00) > (12<<10)) { return(0); } // R cannot be > 12
+
+  // Get the length
+  u16 = (Data[3]&0x03);
+  u16 = (u16<<8) | Data[4];
+  u16 = (u16<<3) | ((Data[5]>>5)&0x07);
+  if (u16 < 7) { return(0); }
+  return(u16);
+} /* _SealIsAACframe() */
+
+/**************************************
  _SealWalkAAC(): Scan the AAC and find the offset to insert.
  Returns:
    Offset is in '@InsertOffset'
@@ -39,28 +113,26 @@
 sealfield *	_SealWalkAAC	(sealfield *Args, mmapfile *Mmap)
 {
   size_t offset, scanStart;
-  uint32_t u16;
+  uint16_t u16;
 
   // Check for AAC; permit up to 1K of garbage
   scanStart=offset=0;
-  while(offset+4 <= Mmap->memsize)
+  while(offset+7 <= Mmap->memsize)
     {
-    u16 = readbe16(Mmap->mem+offset);
-    switch(u16)
+    u16 = _SealIsAACframe(Mmap->mem+offset);
+    if (u16==0) { offset++; } // skip non-frame
+    else
 	{
-	case 0xfff0:
-	case 0xfff1:
-	  if (offset > 0) { Args = SealVerifyBlock(Args, scanStart, offset, Mmap); }
-	  scanStart=offset;
-	  offset+=2;
-	  break;
-	default:
-	  offset++;
-	  break;
+	// verify data between frames
+	if (scanStart < offset) { Args = SealVerifyBlock(Args, scanStart, offset, Mmap); }
+	offset += u16;
+	scanStart = offset;
 	}
     }
-  if (offset > 0) { Args = SealVerifyBlock(Args, scanStart, offset, Mmap); }
-  Args = SealSetIindex(Args,"@InsertOffset",0,Mmap->memsize);
+  // Scan to end of file
+  if (scanStart < Mmap->memsize) { Args = SealVerifyBlock(Args, scanStart, Mmap->memsize, Mmap); }
+  // Truncated file? Insert after the last frame!
+  Args = SealSetIindex(Args,"@InsertOffset",0,(offset > Mmap->memsize) ? offset : Mmap->memsize);
   return(Args);
 } /* _SealWalkAAC() */
 
@@ -72,14 +144,9 @@ sealfield *	_SealWalkAAC	(sealfield *Args, mmapfile *Mmap)
  **************************************/
 bool	Seal_isAAC	(mmapfile *Mmap)
 {
-  uint16_t u16;
-
   if (!Mmap || (Mmap->memsize < 40)) { return(false); } // too small
-
   // Must begin with a frame
-  u16 = readbe16(Mmap->mem+0);
-  if ((u16 == 0xfff0) || (u16 == 0xfff1)) { return(true); }
-  
+  if (_SealIsAACframe(Mmap->mem) > 0) { return(true); } // valid frame
   return(false);
 } /* Seal_isAAC() */
 
@@ -143,7 +210,7 @@ sealfield *	Seal_AACsign	(sealfield *Args, mmapfile *MmapIn)
   Args = SealRecord(Args); // get placeholder
 
   // Create the block
-  Args = SealSetBin(Args,"@BLOCK",4,(const byte*)"\x00\x00\x00\x00"); // for safety, pad.
+  Args = SealSetBin(Args,"@BLOCK",1,(const byte*)"\x00"); // for safety, pad.
 
   // Make '@s' relative to block
   rec = SealSearch(Args,"@BLOCK");
@@ -155,7 +222,7 @@ sealfield *	Seal_AACsign	(sealfield *Args, mmapfile *MmapIn)
   Args = SealAddBin(Args,"@BLOCK",rec->ValueLen,rec->Value);
   Args = SealAddText(Args,"@BLOCK","\n");
 
-  Args = SealAddBin(Args,"@BLOCK",4,(const byte*)"\x00\x00\x00\x00"); // for safety, pad.
+  Args = SealAddBin(Args,"@BLOCK",1,(const byte*)"\x00"); // for safety, pad.
   SealSetType(Args,"@BLOCK",'x');
  
   MmapOut = SealInsert(Args,MmapIn,InsertOffset);
