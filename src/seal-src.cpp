331480c9eb84015f61a05e8d132be31bcd7173d2
Miranda <flowerbutterfly8@gmail.com>
2025-09-22 21:33:40 -0400

    need to do more cleanup and testing

--- src/seal-src.cpp
+++ src/seal-src.cpp
@@ -53,8 +53,90 @@ size_t	SealCurlSrcCallback	(void *buffer, size_t size, size_t nmemb, void *parm)
   EVP_MD_CTX* ctx64;
   ctx64 = (EVP_MD_CTX*)parm;
   EVP_DigestUpdate(ctx64,buffer,nmemb*size);
-  return(nmemb*size);
+  return(size * nmemb);
 } /* SealCurlCallback() */
+
+/**************************************
+ SealFinalize Digest(): Finalize the Digest
+ **************************************/
+char*   SealFinalizeDigest(sealfield *Args, EVP_MD_CTX* ctx64, char* srcaSf, const EVP_MD* (*mdf)(void)){
+    // Finalize digest
+  unsigned int mdsize;
+  mdsize = EVP_MD_size(mdf()); // digest size
+  Args = SealAlloc(Args,"@srcdCalc",mdsize,'b'); // binary digest
+  EVP_DigestFinal(ctx64,SealSearch(Args,"@srcdCalc")->Value,&mdsize); // store the digest
+  EVP_MD_CTX_free(ctx64);
+  // Re-encode digest from binary to expected srca format.
+  // Currently, only supports base64.
+  if (!strcmp(srcaSf,"base64")) { 
+        SealBase64Encode(SealSearch(Args,"@srcdCalc")); }
+  else if (!strcmp(srcaSf,"hex")) { SealHexEncode(SealSearch(Args,"@srcdCalc"), false); }
+  else if (!strcmp(srcaSf,"HEX")) { SealHexEncode(SealSearch(Args,"@srcdCalc"), true); }
+  else if (!strcmp(srcaSf,"bin")) { ; } // already binary
+  else // unsupported
+    {
+      fprintf(stderr, "ERROR: unknown format for srca (%s)\n", srcaSf);
+      exit(0x80);
+    }
+   return SealGetText(Args,"@srcdCalc");
+}
+
+/**************************************
+ SealCurlSrcCallback(): Call the src URL 
+ and calculate the Digest for the given algorthim
+ **************************************/
+char*	SealGetDigestFromURL	(sealfield *Args, EVP_MD_CTX* ctx64, char* srcaSf, const EVP_MD* (*mdf)(void))
+{
+  sealfield *vf;
+  CURL *ch; // curl handle
+  CURLcode crc; // curl return code
+  char errbuf[CURL_ERROR_SIZE];
+  crc = curl_global_init(CURL_GLOBAL_DEFAULT);
+  if (crc != CURLE_OK)
+    {
+  fprintf(stderr," ERROR: Failed to initialize curl. Aborting.\n");
+  exit(0x80);
+    }
+
+  ch = curl_easy_init();
+  if (!ch)
+    {
+  fprintf(stderr," ERROR: Failed to initialize curl handle. Aborting.\n");
+  exit(0x80);
+    }
+
+  // Ignore TLS cerification?
+  if (SealSearch(Args,"cert-insecure")) { curl_easy_setopt(ch, CURLOPT_SSL_VERIFYPEER, 0L); }
+  else { curl_easy_setopt(ch, CURLOPT_SSL_VERIFYPEER, 1L); }
+
+  // In Cygwin, curl tries to find a cert in /etc, which doesn't exist.
+  // Therefore, include our own cacert from https://curl.se/docs/caextract.html
+  vf = SealSearch(Args,"cacert");
+  if (vf) { curl_easy_setopt(ch, CURLOPT_CAINFO, vf->Value); }
+
+  // Set retrieval parameters
+  curl_easy_setopt(ch, CURLOPT_URL, SealGetText(Args,"src")); // set the URL
+  curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, SealCurlSrcCallback);
+  curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void*)ctx64); // callback gets pointer to open digest
+  memset(errbuf,0,CURL_ERROR_SIZE);
+  curl_easy_setopt(ch, CURLOPT_ERRORBUFFER, errbuf);
+  curl_easy_setopt(ch, CURLOPT_CONNECTTIMEOUT, 20); // 20 seconds to connect
+  curl_easy_setopt(ch, CURLOPT_TIMEOUT, 60); // 60 seconds to transfer data
+
+  // Do the request!
+  crc = curl_easy_perform(ch);
+  curl_easy_cleanup(ch);
+
+  // Clean up
+  curl_global_cleanup();
+  if (crc != CURLE_OK)
+    {
+  fprintf(stderr," ERROR: curl[%d]: %s\n",crc,errbuf[0] ? errbuf : "unknown");
+  exit(0x80);
+    }
+  return SealFinalizeDigest(Args, ctx64, srcaSf, mdf);
+} /* SealGetDigestFromURL() */
+
 
 /**************************************
  SealSrcGet(): Get a src record and validate the record at the src 
@@ -67,7 +149,6 @@ sealfield *	SealSrcGet	(sealfield *Args)
   const EVP_MD* (*mdf)(void);
   char *src,*srcd;
   char *srca;
-  sealfield *vf;
 
   // Get the three main values for this part
   srca = SealGetText(Args,"srca"); 
@@ -77,11 +158,6 @@ sealfield *	SealSrcGet	(sealfield *Args)
   // Validate the input
 
   if (!srcd && !src) { return(Args); } // nothing to do
-  if (!srcd && srca) //the algorithim is present, but not the digest
-    {
-      Args = SealSetText(Args,"@error","undefined srca");
-      return(Args);
-    }
 
   // Process srca
   if(!srca){ //Set it to the default if not specified
@@ -98,62 +174,17 @@ sealfield *	SealSrcGet	(sealfield *Args)
   else
     {
       free(srcaCopy);
-      fprintf(stderr, "ERROR: unknown srca format (%s)\n", srca);
+      fprintf(stderr, "ERROR: unknown srca algorithm (%s)\n", srcaDa);
       exit(0x80);
     }
 
   EVP_MD_CTX* ctx64 = EVP_MD_CTX_new();
   EVP_DigestInit(ctx64, mdf());
-
+  char* srcdCalc;
   // Compute the srcd
    if (src && (strncasecmp(src,"http://",7) == 0 || strncasecmp(src,"https://",8) == 0)) // it's a URL!
     {
-    CURL *ch; // curl handle
-    CURLcode crc; // curl return code
-    char errbuf[CURL_ERROR_SIZE];
-    crc = curl_global_init(CURL_GLOBAL_DEFAULT);
-    if (crc != CURLE_OK)
-      {
-	fprintf(stderr," ERROR: Failed to initialize curl. Aborting.\n");
-	exit(0x80);
-      }
-
-    ch = curl_easy_init();
-    if (!ch)
-      {
-	fprintf(stderr," ERROR: Failed to initialize curl handle. Aborting.\n");
-	exit(0x80);
-      }
-
-    // Ignore TLS cerification?
-    if (SealSearch(Args,"cert-insecure")) { curl_easy_setopt(ch, CURLOPT_SSL_VERIFYPEER, 0L); }
-    else { curl_easy_setopt(ch, CURLOPT_SSL_VERIFYPEER, 1L); }
-
-    // In Cygwin, curl tries to find a cert in /etc, which doesn't exist.
-    // Therefore, include our own cacert from https://curl.se/docs/caextract.html
-    vf = SealSearch(Args,"cacert");
-    if (vf) { curl_easy_setopt(ch, CURLOPT_CAINFO, vf->Value); }
-
-    // Set retrieval parameters
-    curl_easy_setopt(ch, CURLOPT_URL, SealGetText(Args,"src")); // set the URL
-    curl_easy_setopt(ch, CURLOPT_WRITEFUNCTION, SealCurlSrcCallback);
-    curl_easy_setopt(ch, CURLOPT_WRITEDATA, (void*)ctx64); // callback gets pointer to open digest
-    memset(errbuf,0,CURL_ERROR_SIZE);
-    curl_easy_setopt(ch, CURLOPT_ERRORBUFFER, errbuf);
-    curl_easy_setopt(ch, CURLOPT_CONNECTTIMEOUT, 20); // 20 seconds to connect
-    curl_easy_setopt(ch, CURLOPT_TIMEOUT, 60); // 60 seconds to transfer data
-
-    // Do the request!
-    crc = curl_easy_perform(ch);
-    curl_easy_cleanup(ch);
-
-    // Clean up
-    curl_global_cleanup();
-    if (crc != CURLE_OK)
-      {
-	fprintf(stderr," ERROR: curl[%d]: %s\n",crc,errbuf[0] ? errbuf : "unknown");
-	exit(0x80);
-      }
+      srcdCalc = SealGetDigestFromURL(Args, ctx64, srcaSf, mdf);
     }
   else if (SealIsFile(src)) // src is a file!
     {
@@ -167,31 +198,10 @@ sealfield *	SealSrcGet	(sealfield *Args)
     exit(0x80);
   }
 
-  // Finalize digest
-  unsigned int mdsize;
-  mdsize = EVP_MD_size(mdf()); // digest size
-  Args = SealAlloc(Args,"@srcdCalc",mdsize,'b'); // binary digest
-  EVP_DigestFinal(ctx64,SealSearch(Args,"@srcdCalc")->Value,&mdsize); // store the digest
-  EVP_MD_CTX_free(ctx64);
-  // Re-encode digest from binary to expected srca format.
-  // Currently, only supports base64.
-  if (!strcmp(srcaSf,"base64")) { SealBase64Encode(SealSearch(Args,"@srcdCalc")); }
-  else if (!strcmp(srcaSf,"hex")) { SealHexEncode(SealSearch(Args,"@srcdCalc"), false); }
-  else if (!strcmp(srcaSf,"HEX")) { SealHexEncode(SealSearch(Args,"@srcdCalc"), true); }
-  else if (!strcmp(srcaSf,"bin")) { ; } // already binary
-  else // unsupported
-    {
-      fprintf(stderr, "ERROR: unknown srca format (%s)\n", srca);
-      free(srcaCopy);
-      exit(0x80);
-    }
-  char* srcdCalc = SealGetText(Args,"@srcdCalc");
   // Compare calculated digest to the expected
   if(srcd && srcdCalc){
     if(strcmp(srcd, srcdCalc) != 0){
-      fprintf(stderr, "ERROR: srcd (%s) does not match the calculated digest (%s)\n", srcd, srcdCalc);
-      free(srcaCopy);
-      exit(0x80);
+      printf("WARNING: srcd (%s) does not match the calculated digest (%s)\n", srcd, srcdCalc);
     }
   }
 
